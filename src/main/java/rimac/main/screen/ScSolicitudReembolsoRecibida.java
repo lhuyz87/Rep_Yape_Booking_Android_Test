a58e46baf0a2a4107edcf0d7983a71ed480353cb
Candy Espinza Baez <candyespinzabaez@MacBook-Pro-de-svcmdasop.local>
2023-03-13 15:22:16 -0500

    upd reembolso

--- src/main/java/rimac/main/screen/ScSolicitudReembolsoRecibida.java
+++ src/main/java/rimac/main/screen/ScSolicitudReembolsoRecibida.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 
 import io.appium.java_client.AppiumDriver;
 import net.serenitybdd.core.Serenity;
+import rimac.main.object.ObjCommons;
 import rimac.main.util.BaseDriver;
 import rimac.main.util.MobileObjectUtil;
 import rimac.main.util.UtilApp;
@@ -19,6 +20,7 @@ public class ScSolicitudReembolsoRecibida extends BaseDriver{
 private long wdwTimeOut = 300L;
 	
 	protected ObjSolicitudReembolsoRecibida objSolicitudReembolsoRecibida = ObjSolicitudReembolsoRecibida.getInstancia();
+	protected ObjCommons objCommons = ObjCommons.getInstancia();
 	// util
 	protected MobileObjectUtil pageObjectUtil2 = MobileObjectUtil.getInstancia();
 	public static Logger looger = Logger.getLogger(ScSolicitudReembolsoRecibida.class.getName());
@@ -32,13 +34,26 @@ private long wdwTimeOut = 300L;
 	
 	public String  obtenerMensaje() {
 		looger.info("Ultimo Paso");
-		util.esperarElementoVisible(15, objSolicitudReembolsoRecibida.lblSolicitudRecibida);
+		int contador=0;
+		while(element(objSolicitudReembolsoRecibida.lblSolicitudRecibida).isCurrentlyVisible()==false && contador<20){
+			//Permite cerrar el modal de recomendación para calificar la app en la playstore
+			if(element(objCommons.btnCerrarmodal).isCurrentlyVisible()){
+				element(objCommons.btnCerrarmodal).click();
+			}
+			util.esperarSegundos(1);
+			contador++;
+		}
+		String mensajeObtenido = element(objSolicitudReembolsoRecibida.lblSolicitudRecibida).getText();
 		Serenity.takeScreenshot();
-		String mensajeObtenido= objSolicitudReembolsoRecibida.lblSolicitudRecibida.getText();
-		util.esperarElemento(5, objSolicitudReembolsoRecibida.btnIrInicio);
+		while(element(objSolicitudReembolsoRecibida.btnIrInicio).isCurrentlyVisible()==false && contador<10){
+			if(element(objCommons.btnCerrarmodal).isCurrentlyVisible()){
+				element(objCommons.btnCerrarmodal).click();
+			}
+			util.esperarSegundos(1);
+			contador++;
+		}
 		element(objSolicitudReembolsoRecibida.btnIrInicio).click();
 		util.esperarSegundos(5);
-		
 		return mensajeObtenido;
 	}
 	
