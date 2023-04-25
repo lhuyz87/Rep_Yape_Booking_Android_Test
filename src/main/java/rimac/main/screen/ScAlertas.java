9de50cdc3d6eb934dc4a560d9b1805dceff95a22
Candy Espinza Baez <candyespinzabaez@MacBook-Pro-de-svcmdasop.local>
2023-04-25 18:08:05 -0500

    Upd Alertas

--- src/main/java/rimac/main/screen/ScAlertas.java
+++ src/main/java/rimac/main/screen/ScAlertas.java
@@ -13,38 +13,33 @@ public class ScAlertas extends BaseDriver {
     protected ObjPaginaPrincipal objPaginaPrincipal =ObjPaginaPrincipal.getInstancia();
     protected UtilApp util = UtilApp.getInstancia();
 
-    public static Logger looger = Logger.getLogger(ScTusTramites.class.getName());
-
     public void omitirAlertas(int intentos) throws Exception {
-            int contador=0;
-            int i=0;
-            while(i<intentos){
-                if(element(objAlertas.btnHuellaPorAhoraNo).isEnabled()){
-                    element(objAlertas.btnHuellaPorAhoraNo).click();
-                }
-                if(element(objAlertas.btnCerrarModal).isEnabled()){
-                    element(objAlertas.btnCerrarModal).click();
-                }
-                if(element(objAlertas.btnEnteratePorAhoraNo).isEnabled()){
-                    element(objAlertas.btnEnteratePorAhoraNo).click();
-                }
-                if(element(objAlertas.btnCerrarModal).isEnabled()){
-                    element(objAlertas.btnCerrarModal).click();
-                }
-                if(element(objAlertas.btnCerrarEmail).isEnabled()){
-                    element(objAlertas.btnCerrarEmail).click();
-                }
-                if(element(objAlertas.btnEnteratePorAhoraNo2).isEnabled()){
-                    element(objAlertas.btnEnteratePorAhoraNo2).click();
-                }
-                if(element(objPaginaPrincipal.btnEmergencia).isEnabled()){
-                    contador++;
-                }
-                i++;
-                util.esperarSegundos(1);
-                if(contador==3){
-                    break;
-                }
+        int contador=0;
+        int i=0;
+        while(i<intentos){
+            if(element(objAlertas.btnHuellaPorAhoraNo).isCurrentlyVisible()){
+                element(objAlertas.btnHuellaPorAhoraNo).click();
             }
+            if(element(objAlertas.btnCerrarModal).isCurrentlyVisible()){
+                element(objAlertas.btnCerrarModal).click();
+            }
+            if(element(objAlertas.btnCerrarModal).isCurrentlyVisible()){
+                element(objAlertas.btnCerrarModal).click();
+            }
+            if(element(objAlertas.btnCerrarEmail).isCurrentlyVisible()){
+                element(objAlertas.btnCerrarEmail).click();
+            }
+            if(element(objAlertas.btnEnteratePorAhoraNo).isCurrentlyVisible()){
+                element(objAlertas.btnEnteratePorAhoraNo).click();
+            }
+            if(element(objPaginaPrincipal.btnEmergencia).isCurrentlyVisible()){
+                contador++;
+            }
+            i++;
+            util.esperarSegundos(1);
+            if(contador==5){
+                break;
+            }
+        }
     }
 }
