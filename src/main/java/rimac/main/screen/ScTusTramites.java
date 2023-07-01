e365aac13793e4c8ca81cbf9b1d0e7660659427d
CandyEsp <candystephanieespinozabaez@gmail.com>
2023-06-30 20:52:47 -0500

    feat Upd ScTus tramites

--- src/main/java/rimac/main/screen/ScTusTramites.java
+++ src/main/java/rimac/main/screen/ScTusTramites.java
@@ -177,12 +177,13 @@ private long wdwTimeOut = 300L;
 			Point end= new Point((int)(dimension.width*0.5), (int)(dimension.height*0.2));
 			util.esperarSegundos(2);
 			util.esperarElementoClick(5,tramitesObject.lblTramites);
-
-			int xTodos=objCommons.btnTodos.getLocation().getX();
-			int yTodos=objCommons.btnTodos.getLocation().getY();
-			util.doSwipeCoordenadas(appiumDriver(), (xTodos+500), yTodos,xTodos,yTodos, 1000);
-			if(element(tramitesObject.opcSalud).isCurrentlyVisible()){
-				element(tramitesObject.opcSalud).click();
+			if(element(objCommons.btnTodos).isCurrentlyVisible()){
+				int xTodos=objCommons.btnTodos.getLocation().getX();
+				int yTodos=objCommons.btnTodos.getLocation().getY();
+				util.doSwipeCoordenadas(appiumDriver(), (xTodos+500), yTodos,xTodos,yTodos, 1000);
+				if(element(tramitesObject.opcSalud).isCurrentlyVisible()){
+					element(tramitesObject.opcSalud).click();
+				}
 			}
 			int contador=0;
 			while(element(tramitesObject.btnReembolsoSalud).isCurrentlyVisible()==false && contador<8) {
@@ -212,12 +213,13 @@ private long wdwTimeOut = 300L;
 			Point end= new Point((int)(dimension.width*0.5), (int)(dimension.height*0.2));
 			util.esperarSegundos(2);
 			util.esperarElementoClick(5,tramitesObject.lblTramites);
-
-			int xTodos=objCommons.btnTodos.getLocation().getX();
-			int yTodos=objCommons.btnTodos.getLocation().getY();
-			util.doSwipeCoordenadas(appiumDriver(), (xTodos+500), yTodos,xTodos,yTodos, 1000);
-			if(element(tramitesObject.opcSalud).isCurrentlyVisible()){
-				element(tramitesObject.opcSalud).click();
+			if(element(objCommons.btnTodos).isCurrentlyVisible()) {
+				int xTodos = objCommons.btnTodos.getLocation().getX();
+				int yTodos = objCommons.btnTodos.getLocation().getY();
+				util.doSwipeCoordenadas(appiumDriver(), (xTodos + 500), yTodos, xTodos, yTodos, 1000);
+				if (element(tramitesObject.opcSalud).isCurrentlyVisible()) {
+					element(tramitesObject.opcSalud).click();
+				}
 			}
 			int contador=0;
 			while(element(tramitesObject.btnBuscarClinica).isCurrentlyVisible()==false && contador<15) {
@@ -248,12 +250,13 @@ private long wdwTimeOut = 300L;
 			Point end= new Point((int)(dimension.width*0.5), (int)(dimension.height*0.2));
 			util.esperarSegundos(2);
 			util.esperarElementoClick(5,tramitesObject.lblTramites);
-
-			int xTodos=objCommons.btnTodos.getLocation().getX();
-			int yTodos=objCommons.btnTodos.getLocation().getY();
-			util.doSwipeCoordenadas(appiumDriver(), (xTodos+500), yTodos,xTodos,yTodos, 1000);
-			if(element(tramitesObject.opcVehicular).isCurrentlyVisible()){
-				element(tramitesObject.opcVehicular).click();
+			if(element(objCommons.btnTodos).isCurrentlyVisible()) {
+				int xTodos = objCommons.btnTodos.getLocation().getX();
+				int yTodos = objCommons.btnTodos.getLocation().getY();
+				util.doSwipeCoordenadas(appiumDriver(), (xTodos + 500), yTodos, xTodos, yTodos, 1000);
+				if (element(tramitesObject.opcVehicular).isCurrentlyVisible()) {
+					element(tramitesObject.opcVehicular).click();
+				}
 			}
 			int contador=0;
 			while(element(tramitesObject.btnAsisVehiculares).isCurrentlyVisible()==false && contador<8) {
@@ -284,9 +287,9 @@ private long wdwTimeOut = 300L;
 			Point end= new Point((int)(dimension.width*0.5), (int)(dimension.height*0.2));
 			util.esperarSegundos(2);
 			util.esperarElementoClick(5,tramitesObject.lblTramites);
-			if(element(tramitesObject.opcVehicular).isCurrentlyVisible()){
-				element(tramitesObject.opcVehicular).click();
-			}
+			if (element(tramitesObject.opcVehicular).isCurrentlyVisible()) {
+					element(tramitesObject.opcVehicular).click();
+				}
 			int contador=0;
 			while(element(objChoferReemplazo.opcChoferReemplazo).isCurrentlyVisible()==false && contador<15) {
 				util.doSwipe(appiumDriver(), start, end, 500);
@@ -316,12 +319,13 @@ private long wdwTimeOut = 300L;
 			Point end= new Point((int)(dimension.width*0.5), (int)(dimension.height*0.2));
 			util.esperarSegundos(2);
 			util.esperarElementoClick(5,tramitesObject.lblTramites);
-
-			int xTodos=objCommons.btnTodos.getLocation().getX();
-			int yTodos=objCommons.btnTodos.getLocation().getY();
-			util.doSwipeCoordenadas(appiumDriver(), (xTodos+500), yTodos,xTodos,yTodos, 1000);
-			if(element(tramitesObject.opcVehicular).isCurrentlyVisible()){
-				element(tramitesObject.opcVehicular).click();
+			if(element(objCommons.btnTodos).isCurrentlyVisible()) {
+				int xTodos = objCommons.btnTodos.getLocation().getX();
+				int yTodos = objCommons.btnTodos.getLocation().getY();
+				util.doSwipeCoordenadas(appiumDriver(), (xTodos + 500), yTodos, xTodos, yTodos, 1000);
+				if (element(tramitesObject.opcVehicular).isCurrentlyVisible()) {
+					element(tramitesObject.opcVehicular).click();
+				}
 			}
 			int contador=0;
 			while(element(tramitesObject.btnDescuentoAsistencias).isCurrentlyVisible()==false && contador<8) {
