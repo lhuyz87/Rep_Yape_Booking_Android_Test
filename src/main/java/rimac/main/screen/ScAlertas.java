4a3daddacf123ce546388da673900ff1fceee8b5
CandyEsp <candystephanieespinozabaez@gmail.com>
2023-08-31 23:51:37 -0500

    feat Upd alertas

--- src/main/java/rimac/main/screen/ScAlertas.java
+++ src/main/java/rimac/main/screen/ScAlertas.java
@@ -42,9 +42,9 @@ public class ScAlertas extends BaseDriver {
                     element(objAlertas.btnCerrarModalInspeccion).click();
                 }
 
-                if (element(objAlertas.btnCerrarModalError).isCurrentlyVisible()) {
+               /* if (element(objAlertas.btnCerrarModalError).isCurrentlyVisible()) {
                     element(objAlertas.btnCerrarModalError).click();
-                }
+                }*/
 
             } catch (Exception e) {
                 System.out.println("Error al omitir la alerta");
